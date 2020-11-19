e236662589cc963ab537026ea6c548d979320711
Kevin MacDonald <kevinmacdonald4@gmail.com>
2020-11-18 21:21:28 -0600

    Add in auto opening of web browser

--- launcher.py
+++ launcher.py
@@ -1,27 +1,30 @@
 import boto3
 import json
 import time
+from ssh_client import SSH
 
 class Launcher:
     def __init__(self):
         self.ec2 = boto3.client('ec2')
+        self.ssh = None
 
     def launch_instance(self, size):
         with open("base_config.json") as f:
             config = json.load(f)
 
-        result = self.ec2.run_instances(**config)
-        image_id = result['Instances'][0]['InstanceId']
-
         if size == "small":
-            config['InstanceType'] = "m4.large"
-        elif size == "medium":
             config['InstanceType'] = "m4.xlarge"
+        elif size == "medium":
+            config['InstanceType'] = "m4.2xlarge"
         elif size == "large":
             config['InstanceType'] = "m4.4xlarge"
 
-        status = 'launching'
+        print("Launching instance...")
+        result = self.ec2.run_instances(**config)
+        image_id = result['Instances'][0]['InstanceId']
+
         count = 0
+        status = 'launching'
 
         while status != 'running':
             count += 1
@@ -34,11 +37,26 @@ class Launcher:
 
         public_dns = info['Reservations'][0]['Instances'][0]['PublicDnsName']
 
+        print("Uploading files...")
+        self.ssh = SSH("/Users/kevinmacdonald/.ssh/aws_key.pem", "ubuntu", public_dns)
+        self.ssh.upload_files(["jupyter-comp.yaml"])
+        self.ssh.upload_files(["aws_credentials.env"])
+        self.ssh.upload_files(["/Users/kevinmacdonald/.ssh/aws_git"])
+
+        print("Launching docker...")
+        self.ssh.execute_commands([
+            "sudo docker swarm init",
+            "sudo docker stack deploy -c jupyter-comp.yaml jup"
+            ])
+
         print(f"ssh -i ~/.ssh/aws_key.pem ubuntu@{public_dns}")
         print(public_dns)
 
-        return image_id
+        return image_id, public_dns
 
     def stop_instance(self, image_id):
+        self.ssh.execute_commands([
+            "sudo docker stack rm jup"
+            ])
         self.ec2.terminate_instances(InstanceIds=[image_id])
 
